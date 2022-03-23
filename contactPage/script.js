6589416a46c2a3b1a2cb772972eb13babf84d22f
tibononoX <thibault-carre@hotmail.fr>
2022-03-23 09:43:37 +0100

    added headerFooter

--- contactPage/script.js
+++
@@ -1,7 +0,0 @@
-//Button Submit
-const message ="Thank you for your email, the team will answer you quickly";
-
-document.addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert(message);
-})
